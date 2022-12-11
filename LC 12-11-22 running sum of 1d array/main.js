81c2617509ea4e5162e41dfe292b8788759d1e92
Albertus Chua <albertus.chua@gmail.com>
2022-12-11 12:53:24 -0500

    Coding challenge for 12/11/22

---
+++ LC 12-11-22 running sum of 1d array/main.js
@@ -0,0 +1,35 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+// Example 3:
+
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+
+// **Solution**
+
+var runningSum = function(nums) {
+    // Make a currentValue variable of 0
+    // Loop through the array
+    // At each index, find the sum of all the nums up to that index
+    
+    let currentTotal = 0;
+  
+    return nums.map((number, i) => {
+        currentTotal = currentTotal + number;
+        return currentTotal;
+    })
+};
